848673bf8e9d32b43d6ccee14bc4a4ebcd990306
Gold <kyusang200@naver.com>
2022-10-14 17:53:17 +0900

    add models to models.py

--- backend/CodingTest/mainApp/models.py
+++ backend/CodingTest/mainApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Lecture(models.Model):
+    lectureId = models.IntegerField(primary_key=True)
+    lectureName = models.TextField(default='')
+
+class Question(models.Model):
+    questionId = models.IntegerField(primary_key=True)
+    lectureId = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+
+class Code_Saved(models.Model):
+    code_savedId = models.IntegerField(primary_key=True)
+    questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Code_Submitted(models.Model):
+    code_submittedId = models.IntegerField(primary_key=True)
+    questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Testcase(models.Model):
+    testcasId = models.IntegerField(primary_key=True)
+    questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+
