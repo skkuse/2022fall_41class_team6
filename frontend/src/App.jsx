6bd905da65eaa696c4b008639daa754187844308
unqeye <salyuan@naver.com>
2022-10-28 14:02:25 +0900

    feat: 3-column layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import { CssBaseline } from '@mui/material';
+import React, { useState } from 'react';
+import { CssBaseline, Grid } from '@mui/material';
 
 export default function App() {
+  const [leftWidth, setLeftWidth] = useState(4);
+  const [rightWidth, setRightWidth] = useState(4);
+
+  const toggleLeft = () => {
+    setLeftWidth(leftWidth === 4 ? 1 : 4);
+  };
+
+  const toggleRight = () => {
+    setRightWidth(rightWidth === 4 ? 1 : 4);
+  };
+
   return (
-    <CssBaseline />
+    <CssBaseline>
+      <Grid container>
+        <Grid item xs={leftWidth}>
+          <h1>Left</h1>
+        </Grid>
+        <Grid item xs={12 - leftWidth - rightWidth}>
+          <h1>Center</h1>
+        </Grid>
+        <Grid item xs={rightWidth}>
+          <h1>Right</h1>
+        </Grid>
+      </Grid>
+    </CssBaseline>
   );
 }
